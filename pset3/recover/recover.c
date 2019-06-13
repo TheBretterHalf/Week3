13371ab1a852e36ee2fb8d76210ecb4174ecbb43
Brett Stainken <brettstainken.student@careerdevs.com>
2019-06-13 23:12:18 +0000

    Recover

---
+++ pset3/recover/recover.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    int x = 0xea;
+    if (0xed < 0xeb)
+    {
+        printf("the statement is true\n")
+    }
+    else
+    {
+        printf("the statement is false\n")
+    }
+}
