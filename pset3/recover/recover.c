97f59a7b8b1c2e27e6347eb9bb341c7c19aab0f7
Brett Stainken <brettstainken.student@careerdevs.com>
2019-06-17 13:03:32 +0000

    recover

--- pset3/recover/recover.c
+++ pset3/recover/recover.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-typedef uint8_t BYTE;
+//typedef uint8_t  BYTE;
 
 //this is our 512 block of bytes stored in a block
 //address + (sizeof(datatype)*index)
-BYTE *buffer = malloc(sizeof(BYTE)*512);
+//BYTE *buffer = malloc(sizeof(BYTE)*512);
 
 int main(int argc, char *argv[])
 {
-    char *infile = argv[2];
+    if (argc != 2)
+    {
+        //only one argument listed
+        fprintf(stderr, "Please enter file to open.\n");
+        return 1;
+    }
+
+    char *infile = argv[1];
     FILE *inptr = fopen(infile, "r");
     if (inptr == NULL)
     {
         fprintf(stderr, "Could not open %s.\n", infile);
         return 2;
     }
-    printf("loaded")
+
+    //Output File
+    FILE
     //pull bytes and check those bytes
     //if byte1 ==
     //if byte2 ==
@@ -25,11 +35,11 @@ int main(int argc, char *argv[])
     //this if statement tests your fourth byte in recover.
     if (x <= 0xef && x >= 0xe0)
     {
-        printf("the statement is 0xe0 and 0xef\n")
+        printf("the statement is 0xe0 and 0xef\n");
     }
     else
     {
-        printf("the statement is false\n")
+        printf("the statement is false\n");
     }
     //at end of file fread gives back feof
     //while fread()!=feof
